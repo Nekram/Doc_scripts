00d848d61761778211a0e013c0d32cc00a56b10a
brandon <bamarken@alaska.edu>
2014-02-21 14:04:19 -0900

    changed to use mmap instead of new/delete

--- mmapTest/mmapTest.cpp
+++ mmapTest/mmapTest.cpp
@@ -1,10 +1,11 @@
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
 #include <cstring>
 #define PAGE_SIZE 4096
-#define SIZE 2*PAGE_SIZE 
+#define SIZE 4*PAGE_SIZE 
 
 using namespace std;
 
@@ -12,25 +13,25 @@ using namespace std;
 int main()
 {
 
-	unsigned char * ptr;
+	void  * ptr;
 	unsigned char * other;
 	int k;
- std::chrono::time_point<std::chrono::system_clock> start, end;
+ std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 	
 
-//for(int k=0; k<100; ++k)
+for(int k=0; k<100; ++k)
 {
-	 start = std::chrono::system_clock::now();
+	 start = std::chrono::high_resolution_clock::now();
 		int i;
 		for(  i=0; i<1; ++i)
 		{
-			ptr=new unsigned char[SIZE];
-			delete[] ptr;
+			ptr = mmap((void*)0, SIZE, PROT_READ+PROT_WRITE,MAP_ANONYMOUS+MAP_SHARED,-1,0);
+			munmap(ptr,SIZE);
 		}
-end = std::chrono::system_clock::now();
+end = std::chrono::high_resolution_clock::now();
 	chrono::duration<double> dt=end-start;
 	
-	cout << chrono::duration_cast<chrono::microseconds>(dt).count() << endl;
+	cout << chrono::duration_cast<chrono::nanoseconds>(dt).count() << endl;
 	
 	}	
 	return 0;
