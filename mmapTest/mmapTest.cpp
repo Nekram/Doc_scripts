dae5b07968a56e49d836c65b708bf70a96ba03c2
brandon <bamarken@alaska.edu>
2014-03-17 14:20:28 -0800

    added newest versions

--- mmapTest/mmapTest.cpp
+++ mmapTest/mmapTest.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <chrono>
 #include <cstring>
+#include <thread>
 #define PAGE_SIZE 4096
 #define SIZE 4*PAGE_SIZE 
 
@@ -19,7 +20,7 @@ int main()
  std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 	
 
-for(int k=0; k<100; ++k)
+for(int k=0; k<1000000; ++k)
 {
 	 start = std::chrono::high_resolution_clock::now();
 		int i;
@@ -28,11 +29,13 @@ for(int k=0; k<100; ++k)
 			ptr = mmap((void*)0, SIZE, PROT_READ+PROT_WRITE,MAP_ANONYMOUS+MAP_SHARED,-1,0);
 			munmap(ptr,SIZE);
 		}
-end = std::chrono::high_resolution_clock::now();
+	end = std::chrono::high_resolution_clock::now();
 	chrono::duration<double> dt=end-start;
 	
-	cout << chrono::duration_cast<chrono::nanoseconds>(dt).count() << endl;
 	
+	cout << chrono::duration_cast<chrono::nanoseconds>(dt).count() << endl;
+	this_thread::sleep_for(chrono::milliseconds(1));
 	}	
 	return 0;
 }
+
